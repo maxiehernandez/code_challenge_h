7b000d9b079f0e8359f4590d7b8444754cd74650
Maxie Hernandez <maxinehernandez@hotmail.com>
2016-10-03 08:38:58 -0400

    Imported songs from ajax into HTML.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,42 +17,98 @@
 //= require_tree .
 
 
-// AJAX call with HTML styling for albums
-function mockUp(data) {
+// HTML styling for albums
+function albumMockUp(data) {
   return '<li>' +
-      '<div class="album-pic">' +
-        '<img src="' + data.cover_photo_url + '">' +
-      '</div>' +
-      '<div id="album-name"> ' + data.name + '</div>' +
-      '<div id="artist-name"> ' + data.artist_name + '</div>' +
-    '</li>';
+  '<div class="album-pic">' +
+  '<img src="' + data.cover_photo_url + '">' +
+  '</div>' +
+  '<div id="album-name"> ' + data.name + '</div>' +
+  '<div id="artist-name"> ' + data.artist_name + '</div>' +
+  '</li>';
 }
 
+// AJAX call for albums
 $(function() {
   $.ajax({
     url: "https://stg-resque.hakuapp.com/albums.json",
     method: "GET",
     dataType: 'jsonp'
   })
-
+  // Appending the json data to the HTML styling
   .done(function(data) {
     $.each(data, function(index, value) {
-      $('#boutique').append(mockUp(value));
+      $('#boutique').append(albumMockUp(value));
     })
   })
+
+  // Styling of Carousel
   .success(function() {
     $('#boutique').boutique({
-  		container_width: 800,
-  		front_img_width: 200,
+      container_width: 800,
+      front_img_width: 200,
       front_img_height: 200,
-  		frames: 5,
+      frames: 5,
       behind_size: 1,
       back_size: 1,
-  		behind_opacity: 1,
+      behind_opacity: 1,
       back_opacity: 1,
       front_topmargin: 70,
       behind_topmargin: 35,
       back_topmargin: 0
-  	});
+    });
   });
 })
+
+
+// HTML styling for songs
+function songMockUp(data) {
+  var genre = "";
+  if ( data.song_label == null ) {
+    genre = "";
+  }else {
+    $.each(data.song_label, function(index, value){
+      genre += '<span class="song-label">' + value + '</span>';
+    })
+  }
+  return '<tr>' +
+  '<th scope="row" class="song-order-spacing"><div class="song-order">' + data.id + '</div></th>' +
+  '<td class="favorite-spacing"><span class="glyphicon glyphicon-star" aria-hidden="true"></span></td>' +
+  '<td class="song-name">' + data.song_name + genre +
+  '</td>' +
+  '<td class="song-time">' + data.song_duration + '</td>' +
+  '</tr>';
+}
+
+
+// AJAX call for albums
+function getSongs(framenumber) {
+  $.ajax({
+    url: "https://stg-resque.hakuapp.com/songs.json?album_id=" + framenumber,
+    method: "GET",
+    dataType: 'jsonp'
+  })
+  // Emptying the table & appending the json data to the HTML styling
+  .done(function(data) {
+    $('tbody').empty();
+    $.each(data, function(index, value){
+      $('tbody').append(songMockUp(value));
+    })
+  });
+}
+
+// Connecting songs to their album
+function move_callback(anchor, instance, framenumber){
+	getSongs(framenumber);
+}
+
+// Selecting the songs from the album in the center location
+$(document).ready(function (){
+  getSongs(1);
+});
+
+$( "td.favorite-spacing" ).on( "click", function(event) {
+  event.preventDefault();
+  console.log( $( this ) );
+
+});
