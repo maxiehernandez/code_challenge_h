ce191e84026229cb37e26230343bb477444a9e3d
Maxie Hernandez <maxinehernandez@hotmail.com>
2016-10-03 19:08:56 -0400

    Added root path.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'albums#index'
+
   resources :songs
   resources :albums
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
